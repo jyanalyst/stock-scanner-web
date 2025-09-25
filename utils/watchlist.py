e8356fd95d105a9ea260c96714e0a0ffaaafb2c1
jyanalyst <jyanalyst@gmail.com>
2025-09-25 02:55:17 +0000

    25 sep

--- utils/watchlist.py
+++ utils/watchlist.py
@@ -14,7 +14,7 @@ DEFAULT_WATCHLIST = [
     'S56.SI', 'AP4.SI', '544.SI', 'G92.SI', 'Z25.SI',
     'RXS.SI', 'S58.SI', 'EB5.SI', '1MZ.SI', '41O.SI',
     'OYY.SI', 'E3B.SI', 'OU8.SI', 'LCC.SI',
-    'A04.SI', 'ER0.SI', '5WH.SI', '1E3.SI', '5TP.SI', '1L2.SI', 'P52.SI',
+    'A04.SI', 'ER0.SI', '5WH.SI', '1E3.SI', '5TP.SI', 'P52.SI',
     'QES.SI', 'NTDU.SI', 'CLN.SI', 'QC7.SI', 'P8Z.SI', 'RE4.SI', 'DU4.SI'
 ]
 
